14e362b8229cdd4c3e75ec6360dee17c31f1636d
IrinaEnotova <irinkayashina@gmail.com>
2024-01-29 13:02:38 +0400

    feat: add solution to wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
+import { parentPort, workerData } from "worker_threads";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (Math.random() > 0.5) {
+    throw new Error("Special error message to check case 'error'");
+  }
+  parentPort.postMessage({
+    status: "resolved",
+    data: nthFibonacci(workerData),
+  });
 };
 
 sendResult();
