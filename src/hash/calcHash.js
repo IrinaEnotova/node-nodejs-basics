008c67995c89df99981510edbd175825a0feda3d
IrinaEnotova <irinkayashina@gmail.com>
2024-01-29 12:59:50 +0400

    feat: add solution to hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createHash } from "crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(pathToFile, "utf8");
+
+  stream.on("data", (data) => {
+    const result = createHash("sha256").update(data).digest("hex");
+    console.log(result);
+  });
+  stream.on("error", (err) => console.log(`Err: ${err.message}`));
 };
 
 await calculateHash();
