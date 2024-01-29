0b5cdf7f42d16e97c52650d2075c2bd49c3d531a
IrinaEnotova <irinkayashina@gmail.com>
2024-01-29 12:58:28 +0400

    feat: add solution to cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+import process from "process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const envEntries = Object.entries(process.env);
+  const rssEntries = envEntries.filter((entry) => {
+    return entry[0].match(/^RSS_/gim);
+  });
+  let result = [];
+  rssEntries.forEach((item) => {
+    result.push(`${item[0]}=${item[1]}`);
+  });
+
+  console.log(result.join("; "));
 };
 
 parseEnv();
